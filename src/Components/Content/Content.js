dc0809777a89a4c549ba16a8f53a2ff22f8c210a
GrayHat12 <grayhathacks10@gmail.com>
2020-09-18 20:47:07 +0530

    Update Content.js

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,4 +1,4 @@
-import { Input } from "antd";
+import { Button, Input } from "antd";
 import React from "react";
 import CustomCard from "../Card/CustomCard";
 import styles from "./Content.module.css";
@@ -12,6 +12,17 @@ class Content extends React.Component {
       x.name.toLowerCase().includes(state.name.toLowerCase())
     );
   };
+  downloadObjectAsJson = () => {
+    var dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(this.props.list));
+    var downloadAnchorNode = document.createElement("a");
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "data.json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  };
   render() {
     return (
       <>
@@ -21,6 +32,9 @@ class Content extends React.Component {
           onChange={(ev) => this.setState({ name: ev.target.value })}
           style={{ marginBottom: 30, width: "70%" }}
         />
+        <Button onClick={this.downloadObjectAsJson}>
+          Download data as JSON
+        </Button>
         <div className={styles.content}>
           {this.filter(this.props.list, this.state).map((v, i) => (
             <CustomCard key={i} data={v} />
